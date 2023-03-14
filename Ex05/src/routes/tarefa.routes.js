b3e513522bd7567bcb04c6d6f4e777a4a57d6f22
brunojokubauskas <brunojokubauskas@gmail.com>
2023-03-14 09:42:19 -0300

    C:/Users/Aluno/AppData/Local/Programs/Git/nova routes

--- Ex05/src/routes/tarefa.routes.js
+++ Ex05/src/routes/tarefa.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const tarefaController = require("../controllers/tarefas");
+const tarefaController = require("../controllers/tarefas.controller");
 
 router.get("/tarefas", tarefaController.getNovaTerafa());
 
