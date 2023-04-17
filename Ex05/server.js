18012e6454586729226c8108a177172196ef996a
brunojokubauskas <brunojokubauskas@gmail.com>
2023-04-17 08:11:26 -0300

    Aula 013

--- Ex05/server.js
+++ Ex05/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.get("/tarefa", tarefaRoutes);
-app.post("/nova", novaRoutes);
+app.get("/tarefa", (req, res) => {
+  // lógica para a rota "/tarefa"
+});
+app.post("/nova", (req, res) => {
+  // lógica para a rota "/nova"
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
